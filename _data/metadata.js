a688023dc704f96a70d94fa360297d537cbea47f
Pete Jobes <pete01@gmail.com>
2023-06-09 16:01:29 +0100

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Pete Jobes",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Human-centred design leader.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Pete Jobes",
+		email: "pete01@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
